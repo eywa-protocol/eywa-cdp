5e78d48035bad5747ce6201c18b0db2661f395a8
philippine-lime <calamansi.media@gmail.com>
2024-06-26 15:45:34 +0700

    receiver test

--- contracts/bridge/Receiver.sol
+++ contracts/bridge/Receiver.sol
@@ -1,62 +1,72 @@
 // SPDX-License-Identifier: UNLICENSED
-// Copyright (c) Eywa.Fi, 2021-2023 - all rights reserved
+// Copyright (c) Eywa.Fi, 2021-2024 - all rights reserved
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "../interfaces/IReceiver.sol";
 import "../interfaces/IAddressBook.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
 
 
 contract Receiver is IReceiver, AccessControlEnumerable {
 
     using Address for address;
 
+    /// @dev bridge role id
     bytes32 public constant BRIDGE_ROLE = keccak256("BRIDGE_ROLE");
+    /// @dev operator role id
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
-    // hash => receives couter
-    mapping(bytes32 => uint8) public receivedHashes;
+    /// @dev hash -> receives count
+    mapping(bytes32 => uint8) public payloadThreshold;
+    /// @dev hash -> data
+    mapping(bytes32 => bytes) public payload;
+    /// @dev protocol -> threshold
+    mapping(address => uint8) public threshold;
+    /// @dev receivers count
+    uint8 public receiversCount;
 
-    // hash => main data
-    mapping(bytes32 => bytes) public mainData;
-    address public addressBook;
-
-    uint8 public threshold = 2;
-
-    constructor(address addressBook_) {
-        require(addressBook_ != address(0), "Receiver: zero address");
+    constructor() {
         _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        addressBook = addressBook_;
+        receiversCount = 1;
     }
 
-    function setAddressBook(address addressBook_) external onlyRole(OPERATOR_ROLE) {
-        require(addressBook_ != address(0), "Receiver: zero address");
-        addressBook = addressBook_;
+    function setThreshold(address sender, uint8 threshold_) external onlyRole(OPERATOR_ROLE) {
+        require(threshold_ >= 1, "Receiver: wrong threshold");
+        require(threshold_ <= receiversCount, "Receiver: wrong threshold");
+        threshold[sender] = threshold_;
     }
 
-    function receiveData(bytes memory receivedData) external onlyRole(BRIDGE_ROLE) {
+    function setReceiversCount(uint8 receiversCount_) external onlyRole(OPERATOR_ROLE) {
+        require(receiversCount_ >= 1, "Receiver: wrong receivers count");
+        receiversCount = receiversCount_;
+    }
+
+    function receiveData(address sender, bytes memory receivedData) external onlyRole(BRIDGE_ROLE) {
+        uint8 threshold_ = threshold[sender];
+        require(threshold_ > 0, "Receiver: threshold is not set");
         bytes32 hash_ = keccak256(receivedData);
-        if (receivedHashes[hash_] >= threshold - 1) {
+        if (payloadThreshold[hash_] >= threshold_) {
             _call(receivedData);
         } else {
-            mainData[hash_] = receivedData;
+            payload[hash_] = receivedData;
         }
-        
+        // TODO delete payload, delete hash if succeeded
     }
 
-    function receiveHashData(bytes memory receivedData) external onlyRole(BRIDGE_ROLE) {
-        if (mainData[bytes32(receivedData)].length != 0) {
-            _call(mainData[bytes32(receivedData)]);
+    function receiveHashData(address sender, bytes memory receivedData) external onlyRole(BRIDGE_ROLE) {
+        if (payload[bytes32(receivedData)].length != 0) {
+            _call(payload[bytes32(receivedData)]);
         }
         else {
-            receivedHashes[bytes32(receivedData)]++;
+            payloadThreshold[bytes32(receivedData)]++;
         }
+        // TODO delete payload, delete hash if succeeded
     }
 
     function _call(bytes memory receivedData) internal {
-        (bytes memory payload, address executor) = abi.decode(receivedData, (bytes, address));
-        (bytes memory data, bytes memory check) = abi.decode(payload, (bytes, bytes));
+        (bytes memory payload_, address executor) = abi.decode(receivedData, (bytes, address));
+        (bytes memory data, bytes memory check) = abi.decode(payload_, (bytes, bytes));
         bytes memory result = executor.functionCall(check);
         require(abi.decode(result, (bool)), "Bridge: check failed");
         executor.functionCall(data, "Receiver: receive failed");
