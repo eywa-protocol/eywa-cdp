cfe583abf70eb44d7441dd57da9afb3379a7e33d
Ildar Absalyamov <i.absalyamov@eywa.fi>
2024-07-02 12:12:41 +0400

    Updated imports

--- contracts/bridge/GovBridgeV2.sol
+++ contracts/bridge/GovBridgeV2.sol
@@ -2,7 +2,7 @@
 // Copyright (c) Eywa.Fi, 2021-2023 - all rights reserved
 pragma solidity ^0.8.17;
 
-import "./BridgeV2.sol";
+import "./send/BridgeV2.sol";
 
 
 contract GovBridgeV2 is BridgeV2 {
